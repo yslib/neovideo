09858cc7d8b929d6238b07d0720631b0c7ab0cfc
Shuoliu Yang <visysl@outlook.com>
2022-06-25 11:17:32 +0800

    update cargo

--- neovideo_vlc/build.rs
+++ neovideo_vlc/build.rs
@@ -1,12 +1,28 @@
+use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
 use std::fs::File;
 use std::path::Path;
-use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let mut file = File::create(&Path::new(&out_dir).join("gl_bindings.rs")).unwrap();
-    Registry::new(Api::Gl, (3, 3), Profile::Core, Fallbacks::All, ["GL_ARB_blend_func_extended"])
-        .write_bindings(GlobalGenerator, &mut file)
-        .unwrap();
+    Registry::new(
+        Api::Gl,
+        (3, 3),
+        Profile::Core,
+        Fallbacks::All,
+        ["GL_ARB_blend_func_extended"],
+    )
+    .write_bindings(GlobalGenerator, &mut file)
+    .unwrap();
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        let vlc_lib_path_dir = env::var_os("VLC_LIB_PATH")
+            .expect("Setup VLC_LIB_PATH for your vlc lib path in order to link")
+            .to_str()
+            .unwrap()
+            .to_owned();
+        println!(r"cargo:rustc-link-search=native={}", vlc_lib_path_dir);
+    }
 }
